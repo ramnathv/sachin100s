af961fec9242e4383ce3b029cf3d077e0abe93e7
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-07-18 23:15:13 -0400

    initial commit

---
+++ code.R
@@ -0,0 +1,79 @@
+require(XML)
+require(RCurl)
+require(lubridate)
+require(plyr)
+require(whisker)
+require(rCharts)
+
+# Get List of Centuries from Wikipedia
+url <- 'http://en.wikipedia.org/wiki/List_of_international_cricket_centuries_by_Sachin_Tendulkar'
+data_ <- readHTMLTable(url, which = 2:3, stringsAsFactors = F)
+
+# Merge Test and ODI Centuries, Standardizing Column Names
+Type <- c('Test', 'ODI')
+data__ <- lapply(1:2, function(i){
+  d <- transform(data_[[i]], 
+    Date = ymd(substr(Date, 2, 11)),
+    Type = Type[i]
+  )
+})
+data__[[1]]$pos = ""
+data__[[1]]$sr = ""
+data__[[2]]$test = ""
+names(data__[[1]]) = c("no", "score", "against", "inn", "test",
+  "venue", "han", "date", "result", "type", "pos", "sr"
+)
+names(data__[[2]]) = c("no", "score", "against", "pos", "inn",
+  "sr", "venue", "han", "date", "result", "type", "test"
+)
+d1 = data__[[1]]
+d2 = data__[[2]][names(d1)]
+d3 = arrange(rbind(d1, d2), date)
+
+# Get Images of Centuries
+url3 <- 'http://omgsachin.blogspot.ca/2011/03/sachin-century-of-centuries.html'
+doc <- htmlParse(url3)
+imgs <- xpathSApply(doc, '//img', xmlGetAttr, 'src')
+d3$imgs <- imgs[grep('[0-9]+\\.jpg$', imgs)]
+d3$result <- gsub('^(.*)\\[[0-9]+\\]$', '\\1', d3$result)
+
+
+# Create Text Template
+tpl <- "
+<b>Against:</b>  {{x.against}}<br/>
+<b>Venue:</b> {{x.venue}}<br/>
+<b>Match:</b> {{x.type}}<br/>
+<b>Score:</b> {{x.score}}<br/>
+<b>Result:</b> <span class={{x.result}}>{{x.result}}</span>
+"
+
+# Create Event Payload for Timeline
+d4 <- alply(d3, 1, function(x){
+  list(
+    startDate = gsub("-", ",", as.character(x$date)),
+    headline = paste("Century No.", x$no),
+    text = whisker.render(tpl, list(x = x)),
+    asset = list(media = x$img)
+  )
+})
+
+# Create Timeline
+m = Timeline$new()
+m$main(
+  headline =  "100 Centuries of Sachin",
+  type = 'default',
+  text = "Sachin",
+  startDate =  "1990,08,14",
+  asset = list(media = 'http://www.hdwallpapers.in/walls/sachin_tendulkar-normal.jpg')
+)
+names(d4) <- NULL
+m$event(d4)
+m$save('index.html')
+
+# Modify JS Path to use Local Assets
+x <- paste(readLines('index.html', warn = F), collapse = '\n')
+x <- gsub('/Library/Frameworks/R.framework/Versions/3.0/Resources/library/rCharts/libraries/timeline', 'compiled', x)
+writeLines(x, con = 'index.html')
+
+# Browse Page
+# browseURL('index.html')
